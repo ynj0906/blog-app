86683f400a5fb20341c37f37e1528576ac01e36c
ynj0906 <n.yasuhito@gmail.com>
2020-01-16 07:58:58 +0900

    1

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', None)
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ["yastoon.com"]
@@ -182,44 +182,44 @@ SITE_ID=1
 ###########
 # Logging #
 ###########
-
-LOGGING = {
-    # バージョンは「1」固定
-    'version': 1,
-    # 既存のログ設定を無効化しない
-    'disable_existing_loggers': False,
-    # ログフォーマット
-    'formatters': {
-        # 本番用
-        'production': {
-            'format': '%(asctime)s [%(levelname)s] %(process)d %(thread)d '
-                      '%(pathname)s:%(lineno)d %(message)s'
-        },
-    },
-    # ハンドラ
-    'handlers': {
-        # ファイル出力用ハンドラ
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': '/var/log/{}/app.log'.format(PROJECT_NAME),
-            'formatter': 'production',
-        },
-    },
-    # ロガー
-    'loggers': {
-        # 自作アプリケーション全般のログを拾うロガー
-        '': {
-            'handlers': ['file'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-        # Django本体が出すログ全般を拾うロガー
-        'django': {
-            'handlers': ['file'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-    },
-}
+#
+# LOGGING = {
+#     # バージョンは「1」固定
+#     'version': 1,
+#     # 既存のログ設定を無効化しない
+#     'disable_existing_loggers': False,
+#     # ログフォーマット
+#     'formatters': {
+#         # 本番用
+#         'production': {
+#             'format': '%(asctime)s [%(levelname)s] %(process)d %(thread)d '
+#                       '%(pathname)s:%(lineno)d %(message)s'
+#         },
+#     },
+#     # ハンドラ
+#     'handlers': {
+#         # ファイル出力用ハンドラ
+#         'file': {
+#             'level': 'INFO',
+#             'class': 'logging.FileHandler',
+#             'filename': '/var/log/{}/app.log'.format(PROJECT_NAME),
+#             'formatter': 'production',
+#         },
+#     },
+#     # ロガー
+#     'loggers': {
+#         # 自作アプリケーション全般のログを拾うロガー
+#         '': {
+#             'handlers': ['file'],
+#             'level': 'INFO',
+#             'propagate': False,
+#         },
+#         # Django本体が出すログ全般を拾うロガー
+#         'django': {
+#             'handlers': ['file'],
+#             'level': 'INFO',
+#             'propagate': False,
+#         },
+#     },
+# }
 
